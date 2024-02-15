fa20ab49b9608a1eaf723625afa5ad81b49af7ec
CoulonTitouan <coulontitouann@gmail.com>
2024-02-15 14:14:09 +0100

    debanmoi en /commande

--- events/debanmoi.js
+++ events/debanmoi.js
@@ -1,11 +1,18 @@
 const { Events } = require('discord.js');
-const client = require("../index.js")
+const client = require("../index.js");
+const fs = require("fs");
 
 module.exports = {
     name: Events.MessageCreate,
     execute(message) {
         if (message.guildId) { return }
-        if (message.content.includes("!debanmoi")) {
+        if (message.content.includes("!debanmoi") && !message.author.bot) {
+            const fichier = "./config/configReglages.json"
+            let configJSON = JSON.parse(fs.readFileSync(fichier, "utf-8"));
+            console.log(configJSON["debanmoi"])
+            if (!configJSON["debanmoi"]) {
+                return message.reply("Le !debanmoi est désactivé.")
+            }
             const Guild = client.guilds.cache.get("1017742904753655828");
             Guild.members.cache.map(async member => {
                 if (member.user.id == message.author.id) {

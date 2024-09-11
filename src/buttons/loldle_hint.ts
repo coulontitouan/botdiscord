ba91b648296729b46b7886079d53f2a878f20812
Livreur <coulontitouann@gmail.com>
2024-09-11 15:52:58 +0200

    hint 1/2

---
+++ src/buttons/loldle_hint.ts
@@ -0,0 +1,9 @@
+import { MessageComponentInteraction } from 'discord.js';
+import { LolDleGame, steps } from '../loldleGame.js';
+
+export default {
+    name: "loldle_hint",
+    async execute(interaction: MessageComponentInteraction) {
+        interaction.reply(LolDleGame.hint(interaction.message.embeds[0].image?.url.match(/\b(champion|spell|item)\b/)?.[0] as keyof typeof steps, interaction.user.id));
+    },
+};  

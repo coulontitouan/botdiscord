e6a694f637777fedca295792afc7605c8166a134
Livreur <coulontitouan@gmail.com>
2025-02-03 19:45:03 +0100

    upload

--- src/commands/upload.ts
+++ src/commands/upload.ts
@@ -1,6 +1,8 @@
-import { SlashCommandBuilder, PermissionsBitField, ChatInputCommandInteraction, GuildMember, PermissionFlagsBits, SlashCommandBooleanOption, SlashCommandAttachmentOption } from 'discord.js';
-import fs from "fs";
+import { SlashCommandBuilder, PermissionsBitField, ChatInputCommandInteraction, GuildMember, PermissionFlagsBits, SlashCommandBooleanOption, SlashCommandAttachmentOption, Attachment } from 'discord.js';
 import { Scopes } from '../constants.js';
+import fs from "fs";
+import axios from "axios"
+import path from 'path';
 
 export default {
     data: new SlashCommandBuilder()
@@ -13,10 +15,57 @@ export default {
         ),
     scope: Scopes.RATIO,
     async execute(interaction: ChatInputCommandInteraction) {
-        console.log(interaction.options.get('fichier', true))
-        interaction.reply({
-            content: `Seuls les admins peuvent désactiver le debanmoi.`,
+        const attachment = interaction.options.get('fichier', true).attachment as Attachment
+        
+        if (attachment.size > 52428800) {
+            return interaction.reply({
+                content: `Le fichier est trop lourd. (max 50Mo)`,
+                ephemeral: true
+            })
+        }
+
+        const url = attachment.url
+        const filename = attachment.name
+        const contentType = attachment.contentType ? attachment.contentType.split(';')[0] : "unknown"
+
+        const outputPath = path.join("/app/files", contentType, filename);
+        // const outputPath = path.join('files', contentType, filename);
+
+        const response = await axios({
+            url,
+            method: 'GET',
+            responseType: 'stream',
+        });
+
+        console.log(outputPath)
+        if (!fs.existsSync(path.dirname(outputPath))) {
+            fs.mkdirSync(path.dirname(outputPath), { recursive: true });
+        }
+
+        await interaction.reply({
+            content: `Upload en cours...`,
             ephemeral: true
-        })
+        });
+
+        try {
+            const writer = fs.createWriteStream(outputPath);
+            response.data.pipe(writer);
+
+            await new Promise((resolve, reject) => {
+                writer.on('finish', () => resolve(undefined));
+                writer.on('error', reject);
+            });
+
+            interaction.editReply({
+                content: `Le fichier a été uploadé avec succès.`,
+            });
+        } catch (error) {
+            console.error(error);
+            interaction.editReply({
+                content: `Une erreur s'est produite lors de l'upload du fichier.`,
+            });
+        }
+
+        return
     },
 }
